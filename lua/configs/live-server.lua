e370a99ea6daa1c3eb6da675d493cd24667e3d0e
Milan <milanmilan.mm55@gmail.com>
2026-04-19 15:52:52 +0200

    updated config and broke plinit into multiple files

--- lua/configs/live-server.lua
+++ lua/configs/live-server.lua
@@ -1,9 +1,25 @@
-local m = {
+return {
+  "selimacerbas/live-server.nvim",
+
+  dependencies = {
+    "folke/which-key.nvim",
+    "nvim-telescope/telescope.nvim", -- recommended for path picker
+  },
+
+  init = function()
+    -- which-key group label only (best practice)
+    local ok, wk = pcall(require, "which-key")
+    if ok then
+      wk.add { { "<leader>l", group = "LiveServer" } }
+    end
+  end,
+
   opts = {
     default_port = 8000,
     live_reload = { enabled = true, inject_script = true, debounce = 120, css_inject = true },
     directory_listing = { enabled = true, show_hidden = false },
   },
+
   keys = {
     { "<leader>ls", "<cmd>LiveServerStart<cr>", desc = "Start (pick path & port)" },
     { "<leader>lo", "<cmd>LiveServerOpen<cr>", desc = "Open existing port in browser" },
@@ -13,6 +29,8 @@ local m = {
     { "<leader>lS", "<cmd>LiveServerStop<cr>", desc = "Stop one (pick port)" },
     { "<leader>lA", "<cmd>LiveServerStopAll<cr>", desc = "Stop all" },
   },
-}
 
-return m
+  config = function(_, opts)
+    require("live_server").setup(opts)
+  end,
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -63,36 +63,7 @@ return {
   },
 
   {
-    "nwiizo/cargo.nvim",
-    build = "cargo build --release",
-    config = function()
-      require("cargo").setup {
-        float_window = true,
-        window_width = 0.8,
-        window_height = 0.8,
-        border = "rounded",
-        auto_close = true,
-        close_timeout = 5000,
-      }
-    end,
-    ft = { "rust" },
-    cmd = {
-      "CargoBench",
-      "CargoBuild",
-      "CargoClean",
-      "CargoDoc",
-      "CargoNew",
-      "CargoRun",
-      "CargoRunTerm",
-      "CargoTest",
-      "CargoUpdate",
-      "CargoCheck",
-      "CargoClippy",
-      "CargoAdd",
-      "CargoRemove",
-      "CargoFmt",
-      "CargoFix",
-    },
+    require "configs.cargo",
   },
 
   {
@@ -106,33 +77,6 @@ return {
   {
     "MunifTanjim/prettier.nvim",
   },
-
-  -- {
-  --   "ray-x/go.nvim",
-  --   dependencies = { -- optional packages
-  --     "ray-x/guihua.lua",
-  --     "neovim/nvim-lspconfig",
-  --     "nvim-treesitter/nvim-treesitter",
-  --   },
-  --   opts = function()
-  --     -- require("go").setup(opts)
-  --     local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-  --     vim.api.nvim_create_autocmd("BufWritePre", {
-  --       pattern = "*.go",
-  --       callback = function()
-  --         require("go.format").goimports()
-  --       end,
-  --       group = format_sync_grp,
-  --     })
-  --     return {
-  --       -- lsp_keymaps = false,
-  --       -- other options
-  --     }
-  --   end,
-  --   event = { "CmdlineEnter" },
-  --   ft = { "go", "gomod" },
-  --   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  -- },
 
   {
     "nvim-treesitter/nvim-treesitter",
@@ -154,30 +98,11 @@ return {
   },
 
   {
-    "selimacerbas/live-server.nvim",
-    dependencies = {
-      "folke/which-key.nvim",
-      "nvim-telescope/telescope.nvim", -- recommended for path picker
-    },
-    init = function()
-      -- which-key group label only (best practice)
-      local ok, wk = pcall(require, "which-key")
-      if ok then
-        wk.add { { "<leader>l", group = "LiveServer" } }
-      end
-    end,
-    opts = require("configs.live-server").opts,
-    keys = require("configs.live-server").keys,
-    config = function(_, opts)
-      require("live_server").setup(opts)
-    end,
+    require "configs.live-server",
   },
 
   {
-    "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
-    cmd = "Trouble",
-    keys = require "configs.trouble",
+    require "configs.trouble",
   },
 
   {
