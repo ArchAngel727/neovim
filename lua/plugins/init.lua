f95fa701501afbc250a0953025f5b29a8a677da3
Milan <milanmilan.mm55@gmail.com>
2026-05-23 15:19:55 +0200

    added comment highlighting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -127,4 +127,10 @@ return {
   {
     require "configs.snacks",
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "BufReadPost",
+    opts = {},
+  },
 }
